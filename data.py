7f47aab2738fed571de5e2d4488b32ca1f687d6e
Adib Mohsin <adibmohsin.root@gmail.com>
2020-11-05 20:07:35 +0600

    more lang

--- data.py
+++ data.py
@@ -1,5 +1,21 @@
 PRE_JSON = """
 {
+	"html": {
+		"extension": ".html",
+		"ids": ["html", "web-html", ".html"]
+	},
+	"css": {
+		"extension": ".css",
+		"ids": ["css", "web-css", ".css"]
+	},
+	"javascript": {
+		"extension": ".js",
+		"ids": ["js", "javascript", ".js"]
+	},
+	"typescript": {
+		"extension": ".ts",
+		"ids": ["ts", "typescript", ".ts"]
+	},
 	"python": {
 		"extension": ".py",
 		"ids": ["py", "python", "python3", ".py"]
@@ -7,6 +23,14 @@ PRE_JSON = """
 	"dart": {
 		"extension": ".dart",
 		"ids": ["dart", "flutter", ".dart"]
+	},
+	"c": {
+		"extension": ".c",
+		"ids": ["c", ".c"]
+	},
+	"c++": {
+		"extension": ".cpp",
+		"ids": ["c++", "cpp", ".cpp"]
 	}
 }
 """
